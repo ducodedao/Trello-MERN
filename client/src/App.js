83efa1ddbb28f84e1985d2e8e5c454f233e20f29
ducodedao <tongodu95@gmail.com>
2022-06-17 18:55:29 +0700

    Client Init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+    return (
+        <div className='App'>
+            <h1>Hello world with React and ducodedao!</h1>
+        </div>
+    )
+}
+
+export default App
